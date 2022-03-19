41f5e61d994995ebafc2f737037dff4f200c97f3
safin777 <sayedislamsafin7770555@gmail.com>
2022-03-19 23:17:54 +0600

    New project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React from 'react';
-import {Route,Routes } from 'react-router-dom';
-import Home from './Component/Home';
-import About from './Component/About';
-import Service from './Component/Service';
-import Contact from './Component/Contact';
-import Policy from './Component/Policy';
-import Error from './Component/Error';
+import React from 'react'
+import Topbar from './Topbar/Topbar'
 
-const App=()=>{
-  return(
+const App = () => {
+  return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/> 
-        <Route path="/about" element={<About/>}/> 
-        <Route path="/service" element={<Service/>}/> 
-        <Route path="/contact" element={<Contact/>}/> 
-        <Route path="/policy" element={<Policy/>}/> 
-        <Route path ="*" element={<Error/>}/> 
-      </Routes>
+      <Topbar/>
     </>
   )
+    
 }
 
-export default App;
+export default App
---
+++ src/Topbar/Topbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './topbar.css'
+
+const Topbar = () => {
+  return (
+    <div className="topbar">
+      <div className="topLeft"> Left </div>
+      <div className="topCenter"> Center </div>
+      <div className="topRight"> Right </div>
+    </div>
+  )
+}
+
+export default Topbar
